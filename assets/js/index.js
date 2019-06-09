c3bf0c7520c6c2d917429ac095a8f3712b81a4c9
vivekiyer114 <vivek@getfrapp.com>
2019-06-09 21:10:15 +0530

    new update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,94 @@
 var MovieApp = (function(){
+    var movieData;
+
     function init(){
         fetchMovieList();
         listenToSearchInput();
+        listenToSortInput();
+        listenToTabClick();
     }
 
+    function listenToSortInput(){
+        var sortEls = document.getElementsByClassName('sort-handler');
+        const L = sortEls.length;
+        for (var c = 0; c < L ; c++){
+            var currSortEl = sortEls[c];
+            currSortEl.addEventListener('click',setActiveSort);
+        }
+    }
+
+    function setActiveSort(e){
+        var el = e.target;
+        var classes = el.classList;
+        var sortType = el.attributes['data-type'].value;
+        
+        var sortFunction = sortType === 'title' ? sortByTitle : sortByYear;
+
+        if (classes.contains('active')){
+            el.classList.remove('active');
+            movieData = movieData.reverse()
+        }else{
+            el.classList.add('active');
+            movieData = movieData.sort((a,b) => sortFunction(a,b,"ascending"))
+        }
+
+        renderMovieList(movieData)
+    }
+
+    function sortByYear(a,b,order){
+        if (order === 'ascending'){
+            if (b.Year > a.Year)return -1;
+            else if (a.Year < b.Year)return 1;
+            else return 0
+        }else{
+            if (b.Year > a.Year)return 1;
+            else if (a.Year < b.Year)return -1;
+            else return 0
+        }
+    }
+
+    function sortByTitle(a,b,order){
+        
+        var btitle = b.Title.toLowerCase();
+        var atitle = a.Title.toLowerCase();
+        
+        if (order === 'ascending'){
+            if (btitle > atitle)return -1;
+            else if (atitle < btitle)return 1;
+            else return 0
+        }else{
+            if (btitle > atitle)return 1;
+            else if (atitle < btitle)return -1;
+            else return 0
+        }
+    }
+
+    function listenToTabClick(){
+        var tabEls = document.querySelectorAll('.navigation li');
+        const L = tabEls.length;
+        for (var c = 0; c < L ; c++){
+            var currTabEl = tabEls[c];
+            currTabEl.addEventListener('click',setActiveTab);
+        }
+    }
+
+    function setActiveTab(e){
+        var el = e.target;
+        var tabEls = document.querySelectorAll('.navigation li');
+        let type = el.attributes['data-type'].value;
+
+        for (var c = 0; c < tabEls.length ; c++){
+            var currentEl = tabEls[c];
+            
+            if (currentEl.attributes['data-type'].value === type){
+                currentEl.setAttribute('class','active')
+            }else{
+                currentEl.setAttribute('class','')
+            }
+        }
+    }
+
+    
     function listenToSearchInput(){
         var formEl = document.getElementsByTagName('form')[0];
         formEl.onsubmit = (e) => e.preventDefault(); 
@@ -24,6 +109,7 @@ var MovieApp = (function(){
 
     function renderMovieList(movieList){
         if (!movieList)return;
+        movieData = movieList;  // Store the list globally
 
         var movieListContainer = document.getElementsByClassName('list-wrapper')[0];
         var listEls = '';
@@ -66,17 +152,3 @@ var MovieApp = (function(){
 MovieApp.init();
 
 
-function setActiveTab(el){
-    var tabEls = document.querySelectorAll('.navigation li');
-    let type = el.attributes['data-type'].value;
-    
-    for (var c = 0; c < tabEls.length ; c++){
-        var currentEl = tabEls[c];
-        
-        if (currentEl.attributes['data-type'].value === type){
-            currentEl.setAttribute('class','active')
-        }else{
-            currentEl.setAttribute('class','')
-        }
-    }
-}
